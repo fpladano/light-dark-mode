3e2c10b17bf518f73b2a3fbb8d862cfc5810d403
Francisco <ip4c0@icloud.com>
2021-09-15 12:35:01 -0500

    Light & Dark

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ const currentTheme = localStorage.getItem("theme");
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
   if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
     darkMode();
   }
 }
