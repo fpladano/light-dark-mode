712fba68a9d9d6a113b2b5ae585218959d76ea44
Francisco <ip4c0@icloud.com>
2021-09-15 14:30:08 -0500

    Light & Dark

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById("nav");
 const toggleIcon = document.getElementById("toggle-icon");
+
 const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
+
 const textBox = document.getElementById("text-box");
 
 function imageMode(color) {
@@ -18,12 +20,7 @@ function darkMode() {
   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
   toggleIcon.children[0].textContent = "Dark Mode";
-  //   toggleIcon.children[1].classList.remove("fa-sun");
-  // toggleIcon.children[1].classList.add("fa-moon");
   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-  //   image1.src = "img/undraw_proud_coder_dark.svg";
-  //   image2.src = "img/undraw_feeling_proud_dark.svg";
-  //   image3.src = "img/undraw_conceptual_idea_dark.svg";
   imageMode("dark");
 }
 
